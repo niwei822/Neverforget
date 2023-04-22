d7376796a7af72046609a8e864d30e298137bffd
niwei822 <cecily.li.822@gmail.com>
2023-04-22 13:29:46 -0700

    refactor map view

--- Neverforget/Controllers/ViewControllers/Dashboard/Map/LocationSearchTableViewController.swift
+++ Neverforget/Controllers/ViewControllers/Dashboard/Map/LocationSearchTableViewController.swift
@@ -8,66 +8,93 @@
 import UIKit
 import MapKit
 
-protocol HandleMapSearch {
-    //bs post the dialogue bubble of the pin.
-    func dropPinZoomIn(placemark:MKPlacemark)
-}
-
 class LocationSearchTableViewController: UITableViewController {
     
-    var matchingItems:[MKMapItem] = []
-    //a handle to the map from the previous screen
-    var mapView: MKMapView? = nil
-    var handleMapSearchDelegate:HandleMapSearch? = nil
+    // Array to store the search results
+    var matchingItems: [MKMapItem] = []
+    
+    // Map view property used to display search results
+    weak var mapView: MKMapView?
+    
+    // Delegate property to handle map search events
+    var handleMapSearchDelegate: HandleMapSearch? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-//Protocol to update search results based on user's search bar input
-extension LocationSearchTableViewController : UISearchResultsUpdating {
-    //Set up the API. call delegate that responds to search bar text entry
+
+// Protocol to update search results based on user's search bar input
+extension LocationSearchTableViewController: UISearchResultsUpdating {
+    
+    // This method is called when the search bar text changes
     func updateSearchResults(for searchController: UISearchController) {
-        guard let mapView = mapView,
-              let searchBarText = searchController.searchBar.text else { return }
+        
+        // Get the mapView and searchBarText from the search controller
+        guard let mapView = mapView, // Map view to search in
+              let searchBarText = searchController.searchBar.text else { return } // Search bar text entered by user
+        
+        // Create a new local search request
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = searchBarText
-        request.region = mapView.region
-        //performs the actual search on the request object.
+        request.naturalLanguageQuery = searchBarText // Set the search query to the user's text input
+        request.region = mapView.region // Set the region to search within to the current mapView region
+        
+        // Create a new local search with the request
         let search = MKLocalSearch(request: request)
-        // responses:an array of mapItems.
-        search.start { [self] response, _ in
-            guard let response = response else {
-                return
-            }
+        
+        // Start the search and wait for a response
+        search.start { [weak self] response, error in
+            
+            // Ensure that self is still in memory and that there are no errors
+            guard let self = self, error == nil,
+                  let response = response else { return }
+            
+            // Set the matchingItems property to the mapItems in the search response
             self.matchingItems = response.mapItems
+            
+            // Reload the table view to show the updated search results
             self.tableView.reloadData()
-            //after this, data come back from API
         }
     }
 }
-//Set up the Table View Data Source
+
+// Set up the Table View Data Source
 extension LocationSearchTableViewController {
+    
+    // Return the number of rows in the table view section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return matchingItems.count
     }
+    
+    // Configure and return a cell for the specified index path
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        //When a search result row is selected, you find the appropriate placemark based on the row number.
-        let selectedItem = matchingItems[indexPath.row].placemark
-        print(selectedItem)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! // Dequeue a reusable cell with the identifier "cell"
+        let selectedItem = matchingItems[indexPath.row].placemark // Get the selected placemark
+        
+        // Set the cell's text label to the placemark's name
         cell.textLabel?.text = selectedItem.name
+        
+        // Create an address string using the placemark's address components
         let address = "\(selectedItem.thoroughfare ?? ""), \(selectedItem.locality ?? ""), \(selectedItem.subLocality ?? ""), \(selectedItem.administrativeArea ?? ""), \(selectedItem.postalCode ?? ""), \(selectedItem.country ?? "")"
+        
+        // Set the cell's detail text label to the address string
         cell.detailTextLabel?.text = address
+        
         return cell
     }
 }
 
 extension LocationSearchTableViewController {
+    
+    // Handle a row being selected in the table view
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedItem = matchingItems[indexPath.row].placemark
+        let selectedItem = matchingItems[indexPath.row].placemark // Get the selected placemark
+        
+        // Call the handleMapSearchDelegate's dropPinZoomIn method with the selected placemark
         handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
-        //Finally, close the search results modal so the user can see the map.
+        
+        // Dismiss the search results table view
         self.dismiss(animated: true, completion: nil)
     }
 }
+
--- Neverforget/Controllers/ViewControllers/Dashboard/Map/MapViewController.swift
+++ Neverforget/Controllers/ViewControllers/Dashboard/Map/MapViewController.swift
@@ -9,71 +9,118 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-    var resultSearchController:UISearchController? = nil
-    var selectedPin:MKPlacemark? = nil
+
+protocol HandleMapSearch {
+    //bs post the dialogue bubble of the pin.
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    // MARK: - Properties
+    private var resultSearchController: UISearchController?
+    private var selectedPin: MKPlacemark?
+    private let locationManager = CLLocationManager()
+    
+    //a handle to the search screen
+    var handleMapSearchDelegate:HandleMapSearch? = nil
     @IBOutlet var map: MKMapView!
-    //give access to location manager
-    var locationManager = CLLocationManager()
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //handle responses asynchronously
+        setupLocationManager()
+        setupSearchController()
+        //map.delegate = self
+    }
+    
+    private func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        //get current location update and call delegate didUpdateLocations
         locationManager.startUpdatingLocation()
-        map.delegate = self
-        //locationsearchtable serve as the searchResultsUpdater delegate
+    }
+    
+    private func setupSearchController() {
+        // Instantiate the LocationSearchTableViewController from the storyboard.
         let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTableViewController
+        
+        // Create a UISearchController with the LocationSearchTableViewController as the search results controller.
         resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        
+        // Set the search results updater to the LocationSearchTableViewController.
         resultSearchController?.searchResultsUpdater = locationSearchTable
-        //configures the search bar
+        
+        // Get the search bar from the search controller and customize it.
         let searchBar = resultSearchController!.searchBar
         searchBar.sizeToFit()
         searchBar.searchTextField.font = UIFont(name: "Deysia Brush", size: 15)
         searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "Search for stores:", attributes: [NSAttributedString.Key.foregroundColor: UIColor.purple])
+        
+        // Set the search controller as the navigation item's search controller.
         navigationItem.searchController = resultSearchController
+        
+        // Configure the search controller to show the navigation bar while searching and obscure the background.
         resultSearchController?.hidesNavigationBarDuringPresentation = false
-        resultSearchController?.obscuresBackgroundDuringPresentation  = true
+        resultSearchController?.obscuresBackgroundDuringPresentation = true
+        
         definesPresentationContext = true
-        //passes along a handle of the mapView
+        
+        // Set the map view and handleMapSearchDelegate properties of the LocationSearchTableViewController.
         locationSearchTable.mapView = map
         locationSearchTable.handleMapSearchDelegate = self
     }
-    //get an array of locations
-    //Zoom to the user’s current location
-    //Tells the delegate that new location data is available.
+}
+
+//implements the CLLocationManagerDelegate protocol to handle location updates
+extension MapViewController: CLLocationManagerDelegate {
+    
+    // This method is called when new location data is available
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         if let location = locations.first {
-            //print(location)
+            // Define the amount of map area to display around the user's location
             let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            
+            // Define the region of the map to display based on the user's location and the span
             let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            
             map.setRegion(region, animated: true)
         }
     }
 }
 
 extension MapViewController: HandleMapSearch {
-    //Then you pass the placemark to the map controller via the custom protocol method.
-    func dropPinZoomIn(placemark:MKPlacemark){
-        // cache the pin
+    // This function is called when a location is selected from the search results
+    func dropPinZoomIn(placemark: MKPlacemark) {
+        // Set the selectedPin property to the placemark that was passed in
         selectedPin = placemark
-        // clear existing pins
+        
+        // Remove any existing annotations from the map
         map.removeAnnotations(map.annotations)
+        
+        // Create a new annotation and set its coordinate and title
         let annotation = MKPointAnnotation()
         annotation.coordinate = placemark.coordinate
         annotation.title = placemark.name
+        
+        // If the placemark has a locality, sub-thoroughfare, thoroughfare, and administrative area,
+        // set the annotation's subtitle to include all of those elements
         if let city = placemark.locality,
            let number = placemark.subThoroughfare,
            let street = placemark.thoroughfare,
            let state = placemark.administrativeArea {
             annotation.subtitle = "\(number) \(street),\(city), \(state)"
         }
-        //To initialize the bubble.
+        
+        // Add the new annotation to the map
         map.addAnnotation(annotation)
+        
+        // Set the map's region to show the selected location
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
         map.setRegion(region, animated: true)
+        
+        // Stop updating the user's location
         locationManager.stopUpdatingLocation()
     }
 }
